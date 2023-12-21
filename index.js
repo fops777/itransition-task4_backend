7d20977f750ecfcd454b16bc707f53dcf404f7f1
fops777 <huzin.ayratik@gmail.com>
2023-12-21 15:06:56 +0500

    11fix

--- index.js
+++ index.js
@@ -123,15 +123,15 @@ app.post("/register", registerValidation, async (req, res) => {
 
 app.get("/users", async (req, res) => {
   // res.json("heey");
-  const users = await UserModel.find(); // Используем метод find для получения всех пользователей из базы данных
-  res.json(users);
-  // try {
-  // } catch (error) {
-  //   console.log(error);
-  //   res.status(500).json({
-  //     message: "Не удалось получить юзеров",
-  //   });
-  // }
+  try {
+    const users = await UserModel.find(); // Используем метод find для получения всех пользователей из базы данных
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось получить юзеров",
+    });
+  }
 });
 
 app.delete("/users", async (req, res) => {
